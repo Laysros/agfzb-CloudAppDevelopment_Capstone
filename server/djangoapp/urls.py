c09cd41e18431422b6e231e0261cbbcc95d8ec29
Laysros <laysros@ymail.com>
2023-05-16 22:30:54 -0400

    add

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -25,7 +25,8 @@ urlpatterns = [
     path(route='', view=views.get_dealerships, name='index'),
 
     # path for dealer reviews view
-
+    path('dealer/<int:id>/', views.get_dealer_details, name='dealer_details'),
+    path('dealer/<int:id>/review', views.add_review, name='add_review'),
     # path for add a review view
 
     ####
